a083027d0e057a0c426ab0421ea90021ea9b8fc7
crvn14-cell <crvn14@gmail.com>
2025-09-01 15:52:58 +0200

    projet.py

--- projet.py
+++ projet.py
@@ -79,16 +79,24 @@ def load_finance_data():
 # PAGE 1 : INTRODUCTION
 # ────────────────────────────────────────────────
 if page == "Introduction":
+   from PIL import Image
+
+# Charger l'image locale
+    image = Image.open("titre.jpeg")
+
+# Afficher l'image centrée et propre
     st.markdown(
     """
-    <div style="display: flex; justify-content: center; align-items: center; margin-bottom: 10px;">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/0/0e/Ubisoft_logo.svg" 
-             alt="Ubisoft Logo" width="200">
-    </div>
+    <div style="display: flex; justify-content: center; align-items: center; margin-bottom: 20px;">
     """,
     unsafe_allow_html=True
 )
-    st.title(" 🎮 Ubisoft — Introduction")
+
+# Taille contrôlée pour éviter qu'elle prenne toute la largeur
+    st.image(image, use_container_width=False, width=600)
+
+    st.markdown("</div>", unsafe_allow_html=True)
+
     introduction = """
     Ubisoft est l’un des plus grands éditeurs de jeux vidéo au monde, reconnu pour ses franchises emblématiques telles que *Assassin's Creed*, *Far Cry*, *Just Dance*, *Rainbow Six* ou encore *The Division*. Fondée en 1986 par les frères Guillemot, l’entreprise a longtemps incarné le savoir-faire vidéoludique français. Introduite en Bourse en 1996, Ubisoft connaît une croissance spectaculaire pendant plus de deux décennies, atteignant un sommet historique en 2018 avec une action valorisée à plus de **100 €**.
 
@@ -1796,6 +1804,7 @@ Par ailleurs, Ubisoft gagnerait à repenser ses modèles économiques, en redonn
     """,
     unsafe_allow_html=True
 )
+
 
 
 
